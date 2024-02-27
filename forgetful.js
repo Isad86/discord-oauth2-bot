5313350cfa3b037a33ac6806e8fa1ca2e5243abc
x2sadddDM <114662157+Isad86@users.noreply.github.com>
2024-02-27 09:54:05 +0700

    Update forgetful.js

--- forgetful.js
+++ forgetful.js
@@ -7,10 +7,10 @@ module.exports = {
   client_id: "1105151748672344144",
   client_secret: "ItJl6YMrAiB5VCdEyzgeBFzmBbiOEaGW",
   redirect_uri: "redirect url",
-  footer: "\u0064\u0069\u0073\u0063\u006f\u0072\u0064\u002e\u0067\u0067\u002f\u0065\u0076\u0065\u006e\u0069\u006e\u0067",
-  support: "\u0068\u0074\u0074\u0070\u0073\u003a\u002f\u002f\u0064\u0069\u0073\u0063\u006f\u0072\u0064\u002e\u0067\u0067\u002f\u0065\u0076\u0065\u006e\u0069\u006e\u0067",
-  wehbook: "https://discord.com/api/webhooks/1105620272205271072/GHH8biRBRDUPwWBDnEQzFcuTZ0M3e3bQIfOvSEt6_zKfZ-dyt-ZaAa-j8993dbnn4fDA", //webhook logs
-  owner: ["826509765533630524"], 
-  authLink: "auth link",
-
-}
+  footer: "discord.gg/evening",
+  support: "https://discord.gg/evening",
+  wehbook: "https://discord.com/api/webhooks/1105620272205271072/GHH8biRBRDUPwWBDnEQzFcuTZ0M3e3bQIfOvSEt6_zKfZ-dyt-ZaAa-j8993dbnn4fDA",
+  //webhook logs
+  owner: ["826509765533630524"],
+  authLink: "auth link"
+};
